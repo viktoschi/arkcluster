2efa28bbe28f50dec437a948f47b0fa1bd093a08
viktoschi <12160746+viktoschi@users.noreply.github.com>
2023-02-26 09:05:04 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -64,13 +64,13 @@ fi
 
 if [ ! -f /etc/cron.d/arkdinowipe ]; then
     log "Adding Dino Wipe cronjob (${CRON_DINO_WIPE}) ..."
-    echo "$CRON_DINO_WIPE steam curl -X POST -F "content=Cronjob Dino Wipe on ${SERVERMAP}" "${WEBHOOK}"" > /etc/cron.d/hookarkdinowipe
+    echo "$CRON_DINO_WIPE steam curl -X POST -F "content='Cronjob Dino Wipe on ${SERVERMAP}'" "${WEBHOOK}"" > /etc/cron.d/hookarkdinowipe
     echo "$CRON_DINO_WIPE steam bash -l -c 'arkmanager rconcmd 'DestroyWildDinos''" > /etc/cron.d/arkdinowipe
 fi
 
 if [ ! -f /etc/cron.d/arkrestart ]; then
     log "Adding Restart cronjob (${CRON_RESTART}) ..."
-    echo "$CRON_RESTART steam curl -X POST -F "content=Cronjob Restarting ${SERVERMAP}" "${WEBHOOK}"" > /etc/cron.d/hookarkrestart
+    echo "$CRON_RESTART steam curl -X POST -F "content='Cronjob Restarting ${SERVERMAP}'" "${WEBHOOK}"" > /etc/cron.d/hookarkrestart
     echo "$CRON_RESTART steam bash -l -c 'arkmanager restart'" > /etc/cron.d/arkrestart
 fi 
 
